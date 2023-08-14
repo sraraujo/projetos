237be0d1e253782171e4bc774a5a93068304b3cb
Jonas Araújo <135404598+sraraujo@users.noreply.github.com>
2023-08-13 21:17:20 -0300

    up

--- facil/pedra-papel_tesoura/pedra-papel-tesoura.js
+++ facil/pedra-papel_tesoura/pedra-papel-tesoura.js
@@ -1,8 +1,8 @@
 var resposta = document.getElementById('resposta')
 var escolha = document.getElementsByName('ppt')
 
-var vitoria;
-var derrota;
+var vitoria = 0
+var derrota = 0
 
 function jogar(){
 
@@ -11,32 +11,90 @@ function jogar(){
     // Trocando termos por números:
     // Pedra = 0, Papel = 1.  Tesosura = 2
 
-    if (escolha_maquina == 0){
-        resposta.innerHTML = `Pedra. ${escolha_maquina}`
+    resposta.innerHTML = `<hr>`
+
+    // MINHA ESCOLHA - PEDRA
+    if (escolha[0].checked){
+
+        // PEDRA x PEDRA
+        if (escolha_maquina == 0){
+            resposta.innerHTML += `Pedra (✊🏼) x (✊🏼) Pedra`
+            resposta.innerHTML += `<p id="laranja">EMPATE</p>`
         
-    }else if ( escolha_maquina == 1){
-        resposta.innerHTML = `Papel. ${escolha_maquina}`
+        // PEDRA x PAPEL
+        } else if (escolha_maquina == 1){
+            resposta.innerHTML += `Pedra (✊🏼) x (👋🏼) Papel`
+            resposta.innerHTML += `<p id="vermelho">PERDI</p>`
+            derrota += 1
+        
+        // PEDRA x TESOURA
+        }else if (escolha_maquina == 2){  
+            resposta.innerHTML += `Pedra (✊🏼) x (✂️) Tesoura`
+            resposta.innerHTML += `<p id="verde">VENCI</p>`
+            vitoria += 1
+        }
     
-    }else if (escolha_maquina == 2){
-        resposta.innerHTML = `Tesoura. ${escolha_maquina}`
-    }else{
-        resposta.innerHTML = `erro: ${escolha_maquina}`
+    // MINHA ESCOLHA - PAPEL
+    } else if (escolha[1].checked){
+
+        // PAPEL x PEDRA
+        if (escolha_maquina == 0){
+            resposta.innerHTML += `<p> Papel (👋🏼) x (✊🏼) Pedra</p>`
+            resposta.innerHTML += `<p id="verde">VENCI</p>`
+            vitoria += 1
+        
+        // PAPEL X PAPEL
+        }else if (escolha_maquina == 1){
+            resposta.innerHTML += `<p> Papel (👋🏼) x (👋🏼) Papel</p>`
+            resposta.innerHTML += `<p id="laranja">EMPATE</p>`
+        
+        // PAPEL x TESOURA
+        }else if (escolha_maquina == 2){
+            resposta.innerHTML += `<p> Papel (👋🏼) x (✂️) Tesoura</p>`
+            resposta.innerHTML += `<p id="vermelho">PERDI</p>`
+            derrota += 1
+        }
+
+    // MINHA ESCOLHA + TESOURA
+    }else if (escolha[2].checked){
+        
+        // TESOURA x PEDRA
+        if (escolha_maquina == 0){
+            resposta.innerHTML += `Tesoura (✂️) x (✊🏼) Pedra`
+            resposta.innerHTML += `<p id="vermelho">PERDI</p>`
+            derrota += 1
+        
+        // TESOURA x PAPEL
+        } else if (escolha_maquina == 1){
+            resposta.innerHTML += `Tesoura (✂️) x (👋🏼) Papel`
+            resposta.innerHTML += `<p id="verde">VENCI</p>`
+            vitoria += 1
+        
+        // TESOURA x TESOURA
+        }else if (escolha_maquina == 2){
+            resposta.innerHTML += `Tesoura (✂️) x (✂️) Tesoura`
+            resposta.innerHTML += `<p id="laranja">EMPATE</p>`
+        }
     }
+
+    resposta.innerHTML += `<hr>`
+    resposta.innerHTML += `<span id="verde">Vitória ${vitoria}</span> x <span id="vermelho">${derrota} Derrota</span>`
 }
 
 function validador(){
-    var num = Math.random() * 10
-    num = parseInt(num)
+    var numero = Math.random();
+    numero = parseInt(numero * 10)
 
-    if ( num <= 2){
-        return num
-    
-    } else{
-        num = Math.abs(num - 7)}
-        
-        if (num <= 2){
-            return num
-        } else{
-            validador()
-        }
+    while (numero > 2 ){
+        numero = Math.random() * 10
+        numero = parseInt(numero)
+    }
+
+    return numero
+}
+
+function limpar(){
+    resposta.innerHTML = ''
+    vitoria = 0
+    derrota = 0
 }
